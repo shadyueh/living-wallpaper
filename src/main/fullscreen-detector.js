39a0ae91b9de2d34676feb8679198d855c508be0
Held Grijo <held.grijo@gmail.com>
2026-08-26 19:58:10 -0400

    fix: prevent memory leak in fullscreen detector by reusing ffi callback
    Move ffi.Library call to lazy-init at module level and create the
    EnumWindows callback once (stored in global to prevent GC) instead of
    allocating a new callback on every 2s poll cycle.

--- src/main/fullscreen-detector.js
+++ src/main/fullscreen-detector.js
@@ -1,31 +1,44 @@
 const POLL_INTERVAL_MS = 2000;
 let intervalId = null;
 
+let user32 = null;
+
+function initLibrary() {
+  if (user32) return;
+  const ffi = require('ffi-napi');
+  user32 = ffi.Library('user32.dll', {
+    'EnumWindows':        ['bool', ['pointer', 'int']],
+    'IsWindowVisible':    ['bool', ['pointer']],
+    'GetWindowLongW':     ['int', ['pointer', 'int']],
+  });
+}
+
+let found = false;
+const GWL_STYLE = -16;
+const WS_CAPTION = 0x00C00000;
+const WS_MAXIMIZE = 0x01000000;
+
 function isAnyWindowFullscreen() {
   try {
-    const ffi = require('ffi-napi');
-    const user32 = ffi.Library('user32.dll', {
-      'EnumWindows':        ['bool', ['pointer', 'int']],
-      'IsWindowVisible':    ['bool', ['int']],
-      'GetWindowLongW':     ['int', ['int', 'int']],
-    });
+    initLibrary();
+    found = false;
 
-    let found = false;
-    const GWL_STYLE = -16;
-    const WS_CAPTION = 0x00C00000;
-    const WS_MAXIMIZE = 0x01000000;
-
-    const EnumWindowsProc = ffi.Callback('bool', ['int', 'int'], (hwnd) => {
-      if (!user32.IsWindowVisible(hwnd)) return true;
-      const style = user32.GetWindowLongW(hwnd, GWL_STYLE);
-      const hasCaption = !!(style & WS_CAPTION);
-      const isMaximized = !!(style & WS_MAXIMIZE);
-      if (!hasCaption && isMaximized) {
-        found = true;
-        return false; // stop enumeration
-      }
-      return true;
-    });
+    const EnumWindowsProc = (global._lwEnumWindowsProc =
+      global._lwEnumWindowsProc ||
+      (function () {
+        const ffi = require('ffi-napi');
+        return ffi.Callback('bool', ['pointer', 'int'], (hwnd, _lParam) => {
+          if (!user32.IsWindowVisible(hwnd)) return true;
+          const style = user32.GetWindowLongW(hwnd, GWL_STYLE);
+          const hasCaption = !!(style & WS_CAPTION);
+          const isMaximized = !!(style & WS_MAXIMIZE);
+          if (!hasCaption && isMaximized) {
+            found = true;
+            return false;
+          }
+          return true;
+        });
+      })());
 
     user32.EnumWindows(EnumWindowsProc, 0);
     return found;
