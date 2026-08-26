e007c2fca62fdcf26eea09e3675fb85f552bcd71
Held Grijo <held.grijo@gmail.com>
2026-08-26 19:25:49 -0400

    feat: project scaffolding with electron, ffi-napi, eslint, jest

---
+++ src/shared/constants.js
@@ -0,0 +1,32 @@
+const IPC = {
+  SET_WALLPAPER: 'lw:set-wallpaper',
+  PAUSE_WALLPAPER: 'lw:pause-wallpaper',
+  RESUME_WALLPAPER: 'lw:resume-wallpaper',
+  WALLPAPER_STATUS: 'lw:wallpaper-status',
+  GET_CONFIG: 'lw:get-config',
+  SET_CONFIG: 'lw:set-config',
+  FULLSCREEN_CHANGED: 'lw:fullscreen-changed',
+  SET_VOLUME: 'lw:set-volume',
+  SET_SPEED: 'lw:set-speed',
+  SHOW_UI: 'lw:show-ui',
+  QUIT_APP: 'lw:quit-app',
+};
+
+const DEFAULT_CONFIG = {
+  wallpaper: null,
+  fps: 30,
+  volume: 0,
+  pauseOnFullscreen: true,
+  pauseOnBattery: true,
+  startMinimized: false,
+  hotkey: 'Ctrl+Shift+W',
+};
+
+const WALLPAPER_STATUS = {
+  PLAYING: 'playing',
+  PAUSED: 'paused',
+  STOPPED: 'stopped',
+  ERROR: 'error',
+};
+
+module.exports = { IPC, DEFAULT_CONFIG, WALLPAPER_STATUS };
